da950986ce92c8cf18266d24e1cdc98e2534c88c
svrodela <svrodela@gmail.com>
2024-09-03 15:37:56 -0600

    ejemplo de texto plano a html

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000
 const server = http.createServer((req,res)=>{
     res.statusCode = 200 //ok
     res.setHeader('Content-Type','text-plain')
-    res.end('Servidor funcionando en node \n')
+    res.end("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Ejemplo html</title></head><body><h1>Encabezado</h1><p>parrafo de texto</p></body></html>")
 })
 // mandar llamar el servidor
 server.listen(port, hostname,()=>{
